01f758f2f78726ad87ec9fb272ecad29b0127d28
Tatiana Gazaryan <65111218+tgazaryan@users.noreply.github.com>
2020-07-03 21:52:07 +0300

    Update searchForm.js

--- js/searchForm.js
+++ js/searchForm.js
@@ -1,5 +1,8 @@
 var searchButton = document.querySelector(".search-1");
 var searchForm = document.querySelector(".search");
+var startSearch = document.querySelector(".search-body")
+var visitDates = document.querySelector(".booking-date");
+
 
 searchButton.addEventListener("click", function (evt) {
   if (searchForm.classList.contains("search-body")) {
@@ -7,6 +10,11 @@ searchButton.addEventListener("click", function (evt) {
     searchForm.classList.remove("search-body");
   } else {searchForm.classList.add("search-body");
   }
+});
+startSearch.addEventListener("submit", function (evt) {
+    if (!visitDates) {
+        evt.preventDefault();
+    }
 });
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
