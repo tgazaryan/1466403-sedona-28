cf75ff8c077565e2014051f54c9bc050fb1742ba
Tatiana Gazaryan <65111218+tgazaryan@users.noreply.github.com>
2020-07-06 23:50:37 +0300

    Update search-form.js

--- js/search-form.js
+++ js/search-form.js
@@ -1,14 +1,13 @@
 var searchButton = document.querySelector(".search-1");
 var searchForm = document.querySelector(".search");
 var startSearch = document.querySelector(".search-body")
-var visitDates = document.querySelector(".booking-date");
+var visitDates = startSearch.querySelector(".booking-date");
 
 
 searchButton.addEventListener("click", function (evt) {
   if (searchForm.classList.contains("search-body")) {
     evt.preventDefault();
     searchForm.classList.remove("search-body");
-    searchForm.classList.remove("search-error");
   } else {evt.preventDefault();
     searchForm.classList.add("search-body");
   }
@@ -16,7 +15,7 @@ searchButton.addEventListener("click", function (evt) {
 startSearch.addEventListener("submit", function (evt) {
     if (!visitDates.value) {
         evt.preventDefault();
-        searchForm.classList.add("search-error");
+        startSearch.classList.add("search-error");
     }
 });
 window.addEventListener("keydown", function (evt) {
@@ -24,7 +23,6 @@ window.addEventListener("keydown", function (evt) {
     if (searchForm.classList.contains("search-body")) {
       evt.preventDefault();
       searchForm.classList.remove("search-body");
-      searchForm.classList.remove("search-error");
     }
   }
 });
