acbc80d6647a30c5edb9a1ee5e49c3f2895fc1cd
Tatiana Gazaryan <65111218+tgazaryan@users.noreply.github.com>
2020-07-07 02:12:18 +0300

    Update search-form.js

--- js/search-form.js
+++ js/search-form.js
@@ -1,28 +1,27 @@
 var searchButton = document.querySelector(".search-1");
-var searchForm = document.querySelector(".search");
-var startSearch = document.querySelector(".search-body")
-var visitDates = startSearch.querySelector(".booking-date");
+var searchForm = document.querySelector(".search-body");
+var visitDates = searchForm.querySelector(".booking-date");
 
 
 searchButton.addEventListener("click", function (evt) {
-  if (searchForm.classList.contains("search-body")) {
+  if (searchForm.classList.contains("search-body-show")) {
     evt.preventDefault();
-    searchForm.classList.remove("search-body");
-  } else {evt.preventDefault();
-    searchForm.classList.add("search-body");
+    searchForm.classList.remove("search-body-show");
+  } else {
+    evt.preventDefault();
+    searchForm.classList.add("search-body-show");
   }
 });
-startSearch.addEventListener("submit", function (evt) {
-    if (!visitDates.value) {
-        evt.preventDefault();
-        startSearch.classList.add("search-error");
-    }
+searchForm.addEventListener("submit", function (evt) {
+  if (!visitDates.value) {
+      evt.preventDefault();
+  }
 });
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (searchForm.classList.contains("search-body")) {
+    if (searchForm.classList.contains("search-body-show")) {
       evt.preventDefault();
-      searchForm.classList.remove("search-body");
+      searchForm.classList.remove("search-body-show");
     }
   }
 });
