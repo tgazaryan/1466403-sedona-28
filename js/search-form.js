3dcc05c8a2e91fcf224397d1ef923b9e0f2972df
Tatiana Gazaryan <65111218+tgazaryan@users.noreply.github.com>
2020-07-07 12:20:31 +0300

    Update search-form.js

--- js/search-form.js
+++ js/search-form.js
@@ -1,27 +1,27 @@
 var searchButton = document.querySelector(".search-1");
 var searchForm = document.querySelector(".search-body");
-var visitDates = searchForm.querySelector(".booking-date");
+var arrivalDate = searchForm.querySelector(".arrival");
+var departureDate = searchForm.querySelector(".departure");
 
 
 searchButton.addEventListener("click", function (evt) {
-  if (searchForm.classList.contains("search-body-show")) {
     evt.preventDefault();
-    searchForm.classList.remove("search-body-show");
-  } else {
-    evt.preventDefault();
-    searchForm.classList.add("search-body-show");
-  }
+    searchForm.classList.toggle("search-body-show");
 });
 searchForm.addEventListener("submit", function (evt) {
-  if (!visitDates.value) {
+  if (!arrivalDate.value||!departureDate.value) {
       evt.preventDefault();
-  }
+      searchForm.classList.remove("form-error");
+      searchForm.offsetWidth = searchForm.offsetWidth;
+      searchForm.classList.add("form-error");
+  } 
 });
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (searchForm.classList.contains("search-body-show")) {
       evt.preventDefault();
       searchForm.classList.remove("search-body-show");
+      searchForm.classList.remove("form-error");
     }
   }
 });
